05be8bdd247eb0fa70a8c7fee1204a83dca71cf3
kssp111 <102369152+kssp111@users.noreply.github.com>
2022-03-25 22:57:14 +0800

    Add files via upload

---
+++ statics/js/home.js
@@ -0,0 +1,23 @@
+(function(w) {
+    function enterLine() {
+        document.getElementById("btnEnter1").onclick = function() {
+            window.open('https://11.kssp11.com', "_self");
+        }
+        document.getElementById("btnEnter2").onclick = function() {
+            window.open('https://12.kssp12.com', "_self");
+        }
+        document.getElementById("btnEnter3").onclick = function() {
+            window.open('https://13.kssp13.com', "_self");
+        }
+        document.getElementById("btnEnter4").onclick = function() {
+            window.open('https://14.kssp14.com', "_self");
+        }
+        document.getElementById("btnEnter6").onclick = function() {
+            window.open('https://www.baidu.com/', "_blank");
+        }
+    }
+    function init() {
+        enterLine();
+    }
+    window.addEventListener("DOMContentLoaded", init, !1);
+})(window);

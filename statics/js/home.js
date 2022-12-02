262995bcd8de099a51487c5c21659147c093b76c
kssp111 <102369152+kssp111@users.noreply.github.com>
2022-12-02 17:57:39 +0800

    Update home.js

--- statics/js/home.js
+++ statics/js/home.js
@@ -1,7 +1,7 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://11.kssp11.com', "_self");
+            window.open(url, "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
             window.open('https://12.kssp12.com', "_self");
